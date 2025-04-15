8055590b49e81e3682808ae6094f15ad928a83e0
herniezz <92106489+herniezz@users.noreply.github.com>
2025-04-15 12:20:16 +0200

    Fix deployment issues and update dependencies

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 
 function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename="/ChronoTune">
             <Routes>
                 <Route path="/" element={<HomeScreen />} />
                 <Route path="/game" element={<GameScreen />} />
